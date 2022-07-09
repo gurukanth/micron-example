e51aa32d0a73e728a8feb94f88f0374538b62c43
Gopal <gurukanth>
2022-07-09 18:56:16 +0000

    Initial commit

---
+++ src/test/java/org/venus/micron/MicronExampleTest.java
@@ -0,0 +1,21 @@
+package org.venus.micron;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronExampleTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
